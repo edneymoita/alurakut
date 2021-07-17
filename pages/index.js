2708d71b952ee5ee06555b3a8a1b2f1e5c058e11
edneymoita <edneymoita@gmail.com>
2021-07-17 13:42:44 -0300

    Final da segunda aula

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import React from 'react'
 import styled from 'styled-components'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/Relations'
 
 const Title = styled.h1`
@@ -11,14 +12,30 @@ const Title = styled.h1`
 
 function ProfileSideBar(props) {
   return (
-    <Box>
+    <Box as='aside'>
       <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }}/>
+      <hr />
+
+      <p>
+        <a className='boxLink' href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
   const githubUser = 'edneymoita';
+  const [comunities, setComunities] = React.useState([{
+    id: new Date().toISOString(),
+    title: 'Eu odeio acordar cedo',
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg'
+  }]);
+
   const pessoasFavoritas = [
     'juunegreiros', 
     'omariosouto', 
@@ -42,8 +59,61 @@ export default function Home() {
             </h1>
             <OrkutNostalgicIconSet></OrkutNostalgicIconSet>
           </Box>
+          <Box>
+            <h2 className='subTitle'>Qual comunidade você quer criar?</h2>
+            <form onSubmit={function handleCreateComunity(e){
+              e.preventDefault();
+              const formData = new FormData(e.target);
+
+              const comunity = {
+                  id: new Date().toISOString,
+                  title: formData.get('title'),
+                  image: formData.get('image'),
+              }
+
+              const newComunities = [...comunities, 'Alura Stars']
+              setComunities(newComunities)
+            }}>
+              <div>
+                <input 
+                  placeholder='Qual vai ser o nome da sua comunidade?'
+                  name='title'
+                  aria-label='Qual vai ser o nome da sua comunidade?' 
+                  type='text'
+                />
+              </div>
+              <div>
+                <input 
+                  placeholder='Coloque uma URL para usar de capa'
+                  name='image'
+                  aria-label='Coloque uma URL para usar de capa' />
+              </div>
+
+              <button>
+                Criar comunidade
+              </button>
+            </form>
+          </Box>
         </div>
         <div className='relationsArea' style={{ gridArea: 'relationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className='smallTitle'>
+                Comunidades ({comunities.length})
+            </h2>
+
+            <ul>
+                {comunities.map((itemAtual) => {
+                  return (
+                    <li key={itemAtual.id}>
+                      <a href={`/users/${itemAtual.title}`}>
+                        <img src={itemAtual.image} />
+                        <span>{itemAtual.title}</span>
+                      </a>
+                    </li>
+                  )
+                })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
           <ProfileRelationsBoxWrapper>
             <h2 className='smallTitle'>
               Amigos ({pessoasFavoritas.length})
@@ -52,7 +122,7 @@ export default function Home() {
             <ul>
               {pessoasFavoritas.map((itemAtual) => {
                 return (
-                  <li>
+                  <li key={itemAtual}>
                     <a href={`/users/${itemAtual}`} key={itemAtual}>
                       <img src={`https://github.com/${itemAtual}.png`} />
                       <span>{itemAtual}</span>
